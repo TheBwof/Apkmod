4679fa08e3b7284b5eea0794a9f92d16e10eadff
TheBwof <121462098+TheBwof@users.noreply.github.com>
2023-04-23 17:24:42 +0600

    Update signapk_alpine.sh

--- scripts/signapk_alpine.sh
+++ scripts/signapk_alpine.sh
@@ -4,7 +4,7 @@ KEYTOOL="/usr/lib/jvm/java-1.8-openjdk/bin/keytool"
 JARSIGNER="/usr/lib/jvm/java-1.8-openjdk/bin/jarsigner"
 
 if [ ! -f $HOME/apkmod.keystore ]; then
-    $KEYTOOL -noprompt -dname "CN=mqttserver.ibm.com, OU=ID, O=IBM, L=Hursley, S=Hants, C=GB" -storepass hax4us -genkey -v -keystore $HOME/apkmod.keystore -alias hax4us -keyalg RSA -validity 10000 -keypass hax4us
+    $KEYTOOL -noprompt -dname "CN=mqttserver.ibm.com, OU=ID, O=IBM, L=Hursley, S=Hants, C=GB" -storepass thebwof -genkey -v -keystore $HOME/apkmod.keystore -alias thebwof -keyalg RSA -validity 10000 -keypass thebwof
 fi
 
 exec $JARSIGNER "$@"
