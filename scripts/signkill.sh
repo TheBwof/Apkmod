8157afad77049e4b06d7d8b6ffeabb215c28f62b
TheBwof <121462098+TheBwof@users.noreply.github.com>
2023-04-23 17:26:08 +0600

    Update signkill.sh

--- scripts/signkill.sh
+++ scripts/signkill.sh
@@ -1,3 +1,3 @@
 #!/data/data/com.termux/files/usr/bin/sh 
 
-dalvikvm -cp $HOME/.apkmod/signkill.jar com.hax4us.signkill.Main "$@"
+dalvikvm -cp $HOME/.apkmod/signkill.jar com.thebwof.signkill.Main "$@"
